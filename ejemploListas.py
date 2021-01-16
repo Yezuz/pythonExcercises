8f29abb6eca6d68d3dec2351b5170383c39a1216
Yezuz <jesus.antonio.fuentes.lizama@uabc.edu.mx>
2021-01-15 19:58:07 -0800

    updated listas_example

--- ejemploListas.py
+++ ejemploListas.py
@@ -1,5 +1,4 @@
-Lista = [100, 25, 65, [54, 25, [96, 21, 47]], 52]
-print(Lista[3][2].index(47))
-Lista.append(90)
-print(Lista)
-
+lista = [100, 25, 65, [54, 25, [96, 21, 47]], 52]
+print(lista[3][2].index(47))
+lista.append(90)
+print(lista)
